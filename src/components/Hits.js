77f7cb94687661a9b3583dd502cd8fd090125a59
Your Name <Phillmccormic@gmail.com>
2022-04-15 22:57:09 -0700

    fightLogicHealthBar

--- src/components/Hits.js
+++ src/components/Hits.js
@@ -1,26 +1,24 @@
 import React from "react";
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+const Hits = ({ turnNumber, turn, side }) => {
+  if (side == turn.attacker.side) {
+    const { maxHit, baseHit, finalDamage } = turn.attacker;
 
-const Hits = ({
-  turn,
-  maxHit,
-  finalDamage,
-  hp,
-  newHp,
-  defenceRoll,
-  baseHit,
-  didBlock,
-}) => {
-  return (
-    <div>
-      {turn}. max hit {maxHit}, hp is {hp}, base hit is {baseHit}, final hit is{" "}
-      {finalDamage}, defenceRoll was {defenceRoll}
-      {didBlock ? ", BLOCKED" : ""}, new hp is {newHp}
-    </div>
-  );
+    return (
+      <div>
+        {turnNumber}. max hit {maxHit}, base hit is {baseHit}, final hit is {finalDamage}
+      </div>
+    );
+  } else {
+    const { defenceRoll, didBlock, hitpoints } = turn.defender;
+
+    return (
+      <div>
+        {turnNumber}. defenceRoll was {defenceRoll}
+        {didBlock ? ", BLOCKED" : ""}, new hp is {hitpoints}
+      </div>
+    );
+  }
 };
 
 export default Hits;
--- src/pages/FightLogic/FightLogic.js
+++ src/pages/FightLogic/FightLogic.js
@@ -2,36 +2,42 @@ import React, { useEffect, useState } from "react";
 import "./FightLogic.css";
 import Hits from "../../components/Hits";
 
+function timeout(delay) {
+  return new Promise((res) => setTimeout(res, delay));
+}
+
+function randomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 const FightLogic = () => {
-  const [hitpoints, setHitpoints] = useState(100);
-  const [attack, setAttack] = useState(10);
-  const [defence, setDefence] = useState(10);
-  const [armour, setArmour] = useState(15);
-  const [weaponBoost, setWeaponBoost] = useState(10);
-  const [attackPotion, setAttackPotion] = useState(5);
-  const [shieldPotion, setShieldPotion] = useState(7);
-  const [turns, setTurns] = useState([]);
-  const maxHit = attack + attackPotion + weaponBoost;
-  const defenceEvent = armour + shieldPotion + defence;
+  const [leftFighter, setLeftFighter] = useState({
+    side: "left",
+    isAttacking: Math.random() < 0.5,
+    hitpoints: 100,
+    attack: 10,
+    defence: 10,
+    armour: 15,
+    weaponBoost: 10,
+    attackPotion: 5,
+    shieldPotion: 7,
+  });
+  const [rightFighter, setRightFighter] = useState({
+    side: "right",
+    hitpoints: 100,
+    attack: 10,
+    defence: 10,
+    armour: 15,
+    weaponBoost: 10,
+    attackPotion: 5,
+    shieldPotion: 7,
+  });
+
   const [isFighting, setIsFighting] = useState(false);
-
-  const hpChange = (newHp) => {
-    setHitpoints(newHp);
-  };
-
-  function timeout(delay) {
-    return new Promise((res) => setTimeout(res, delay));
-  }
-
-  // console.log("attack roll =", attack + attackPotion);
-
-  function randomInt(max) {
-    return Math.floor(Math.random() * (max + 1));
-  }
+  const [turns, setTurns] = useState([]);
 
   useEffect(() => {
-    if (isFighting && hitpoints > 0) {
-      console.log(hitpoints);
+    if (isFighting && leftFighter.hitpoints > 0 && rightFighter.hitpoints > 0) {
       doTurn();
     } else {
       setIsFighting(false);
@@ -40,28 +46,42 @@ const FightLogic = () => {
 
   async function doTurn() {
     await timeout(1000);
-    const baseHit = randomInt(maxHit);
+    const currentAttacker = leftFighter.isAttacking ? leftFighter : rightFighter;
+    const { attack, weaponBoost, attackPotion } = currentAttacker;
+    const currentDefender = currentAttacker == leftFighter ? rightFighter : leftFighter;
+    const { hitpoints, defence, armour, shieldPotion } = currentDefender;
+    const defenceEvent = armour + shieldPotion + defence;
     const defenceRoll = randomInt(99);
     const didBlock = defenceRoll < Math.min(defenceEvent, 90);
-    const finalDamage = Math.floor(
-      !didBlock ? baseHit : baseHit * (1 - defence / 100)
-    );
+    const maxHit = attack + attackPotion + weaponBoost;
+    const baseHit = randomInt(maxHit);
+    const finalDamage = Math.floor(!didBlock ? baseHit : baseHit * (1 - defence / 100));
     const newHp = Math.max(hitpoints - finalDamage, 0);
-    // const newHp = hitpoints - finalDamage;
+
     setTurns((old) => [
       ...old,
       {
-        maxHit,
-        hitpoints,
-        defence,
-        baseHit,
-        finalDamage,
-        defenceRoll,
-        didBlock,
-        newHp,
+        attacker: {
+          side: currentAttacker == leftFighter ? "left" : "right",
+          maxHit,
+          baseHit,
+          finalDamage,
+        },
+        defender: {
+          defence,
+          hitpoints: newHp,
+          defenceRoll,
+          didBlock,
+        },
       },
     ]);
-    setHitpoints(newHp);
+
+    if (currentDefender == leftFighter) {
+      setLeftFighter({ ...leftFighter, hitpoints: newHp, isAttacking: true });
+    } else {
+      setRightFighter({ ...rightFighter, hitpoints: newHp });
+      setLeftFighter({ ...leftFighter, isAttacking: false });
+    }
   }
 
   const start = () => {
@@ -80,22 +100,32 @@ const FightLogic = () => {
           FightLogic<button onClick={start}>START FIGHT!</button>
         </div>
       </div>
-      <div className="leftFighter">
-        {turns.map((t, index) => (
-          <Hits
-            turn={index + 1}
-            maxHit={t.maxHit}
-            hp={t.hitpoints}
-            defence={t.defence}
-            baseHit={t.baseHit}
-            finalDamage={t.finalDamage}
-            defenceRoll={t.defenceRoll}
-            didBlock={t.didBlock}
-            newHp={t.newHp}
-          ></Hits>
-        ))}
+      <div className="fighter">
+        <div>
+          {turns.map((t, index) => (
+            <Hits side={"left"} turnNumber={index + 1} turn={t} />
+          ))}
+        </div>
+        <div className="healthbar">
+          <div className="health" style={{ height: `${leftFighter.hitpoints}%` }}>
+            {leftFighter.hitpoints}
+          </div>
+        </div>
       </div>
-      <div className="rightFighter">right</div>
+
+      <div className="fighter">
+        <div>
+          {turns.map((t, index) => (
+            <Hits side={"right"} turnNumber={index + 1} turn={t} />
+          ))}{" "}
+        </div>
+        <div className="healthbar">
+          <div className="health" style={{ height: `${rightFighter.hitpoints}%` }}>
+            {" "}
+            {rightFighter.hitpoints}{" "}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
