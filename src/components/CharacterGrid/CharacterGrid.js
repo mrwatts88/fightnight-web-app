8caba171d145a03fec7d33b3b7d3492d6127d04c
Your Name <Phillmccormic@gmail.com>
2022-04-15 23:21:27 -0700

    fightMusic

--- src/components/CharacterGrid/CharacterGrid.js
+++ src/components/CharacterGrid/CharacterGrid.js
@@ -2,14 +2,10 @@ import React from "react";
 import CharacterGridItem from "../CharacterGridItem/CharacterGridItem";
 import "./CharacterGrid.css";
 
-const imageUrl =
-  "https://thumbs.dreamstime.com/b/fairytale-mage-d-digital-render-fairy-tale-isolated-white-background-46714348.jpg";
-const mageUrl =
-  "https://i.pinimg.com/originals/8d/6f/f3/8d6ff31f94e244db66e9e96bb87dfa70.gif";
-const dwarfUrl =
-  "https://i.pinimg.com/736x/90/61/60/906160ae6ade93cf98ffe31e3ba743e1.jpg";
-const elfUrl =
-  "https://cdna.artstation.com/p/assets/images/images/022/634/826/large/daniel-spagnol-elf-small.jpg?1576149892";
+const imageUrl = "https://thumbs.dreamstime.com/b/fairytale-mage-d-digital-render-fairy-tale-isolated-white-background-46714348.jpg";
+const mageUrl = "https://i.pinimg.com/originals/8d/6f/f3/8d6ff31f94e244db66e9e96bb87dfa70.gif";
+const dwarfUrl = "https://i.pinimg.com/736x/90/61/60/906160ae6ade93cf98ffe31e3ba743e1.jpg";
+const elfUrl = "https://cdna.artstation.com/p/assets/images/images/022/634/826/large/daniel-spagnol-elf-small.jpg?1576149892";
 
 const knights = [{ imageUrl }, "", ""];
 const mages = [{ mageUrl }, { mageUrl }, { mageUrl }];
