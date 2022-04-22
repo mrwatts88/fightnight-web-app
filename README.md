fe3dc62392623c82e6956766118529e65d100630
Matt Watts <matthew.watts.mw@gmail.com>
2022-04-21 20:39:42 -0700

    Add character in city, add stat preview

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 ### FightNight
 
-[x] deploy backend
-[x] put world map in game
-x[] make cities clickable
-x[] make city maps
-[x] Move fight logic to server
+x[] Check stat details
+x[] Integrate character stats into fights
+x[] Make a click target config system
 x[] Integrate swing speed
-[]set/retrieve character base stats
+x[] Make city maps
 [] Loot contract
 [] Staking contract
-[] / stats / health
+[] Check health
 [] Check inventory/ Check character equipment
-[] Go to town center and get herbs
-[] Heal
-x[] Go to fight caves
-[] Select oppenent / fight
+[] Select opponent and fight
+[] Get herbs and heal
 [] mobile view
+[x] Place character in city when visiting
+[x] Click character for stat preview
+[x] deploy backend
+[x] put world map in game
+[x] make cities clickable
+[x] Move fight logic to server
--- src/pages/Aphenos/Aphenos.js
+++ src/pages/Aphenos/Aphenos.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Aphenos.css";
 
 export const Aphenos = () => {
+  const statPreviewRef = useRef(null);
   const history = useNavigate();
   const goBack = () => {
     history("/world-map");
   };
+
+  const showStatPreview = () => {
+    statPreviewRef.current.style.visibility = "visible";
+  };
+
+  const hideStatPreview = () => {
+    statPreviewRef.current.style.visibility = "hidden";
+  };
+
+  const openModal = () => {
+    console.log("modal open");
+  };
+
   return (
     <div className="mapAphenos">
       <div
@@ -21,7 +35,25 @@ export const Aphenos = () => {
       >
         «
       </div>
-      <img className="aphenos" src={"Apheno.jpg"}></img>
+      <div className="aphenosImgWrapper">
+        <div onMouseLeave={hideStatPreview} onMouseOver={showStatPreview} className="meWrapper">
+          <div ref={statPreviewRef} className="statPreview">
+            <div className="flex statPreviewInner">
+              <img src="sword-icon.jpeg" alt="sword" className="sword" />
+              <div>40</div>
+            </div>
+            <div className="flex statPreviewInner">
+              <img src="shield-icon.jpeg" alt="shield" className="shield" />
+              <div>60</div>
+            </div>
+            <div onClick={openModal} className="flex statPreviewInner">
+              <img src="bar-graph.webp" alt="graph" className="graph" />
+            </div>
+          </div>
+          <img src="knight.png" alt="knight" className="me" />
+        </div>
+        <img className="aphenos" alt="city" src={"Apheno.jpg"}></img>
+      </div>
     </div>
   );
 };
